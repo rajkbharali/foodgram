4ffe20b5ef8e731a3c9054de8f8abdeb60403c6d
Raj <rajkbharali@gmail.com>
2024-04-02 13:41:24 +0530

    ep-1

---
+++ App.js
@@ -0,0 +1,11 @@
+const parent = React.createElement("div", {id:"parent"} , 
+    React.createElement("div", {id:"child"},
+    React.createElement("h1", {id:"heading"}, "This is a h1 Tag")
+    )
+)
+
+// const heading = React.createElement("h1", {id:"heading", xyz:"abc"}, "Hello from React")
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(heading)
+root.render(parent)
