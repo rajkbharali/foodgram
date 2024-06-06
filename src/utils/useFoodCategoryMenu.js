e29fc08a1fb1f1172ac1550eec1e0892067c4b2c
rajkbharali <rajkbharali@gmail.com>
2024-06-06 11:13:52 +0530

    added proxy server

--- src/utils/useFoodCategoryMenu.js
+++ src/utils/useFoodCategoryMenu.js
@@ -9,7 +9,7 @@ const useFoodCategoryMenu = (id1, id2) => {
 
   async function fetchFoodList() {
     const data = await fetch(
-      `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&collection=${id2}&tags=layout_CCS_${
+      `https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&collection=${id2}&tags=layout_CCS_${
         id1.split("")[0].toUpperCase() + id1.slice(1)
       }&sortBy=&filters=&type=rcv2&offset=0&page_type=null`
     );
--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -15,7 +15,7 @@ const useRestaurantMenu = (id) => {
 
   async function fetchData() {
     const data = await fetch(
-      `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=${id}`
+      `https://proxy.cors.sh/https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=${id}`
     );
     const json = await data.json();
     setResInfo(json.data);
